fbea86cec459b07d0bcb9e82c867adc1e19cafc4
Oswaldo Casique <pirata_cosmo@hotmail.com>
2023-04-02 12:41:01 -0600

    archvios productos

--- models/role.js
+++ models/role.js
@@ -4,7 +4,7 @@ const { Schema,model }=require('mongoose')
 const RoleSchema = Schema({
     rol:{
         type: String,
-        require:[true,'El rol es obligatorio'],
+        required:[true,'El rol es obligatorio'],
     }
 });
 
